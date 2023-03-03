2c149024a05fc0e942bae0ea68f8a20ca38cf9c8
Yuuuugo <hugogabrielidis@gmail.com>
2023-03-03 15:40:41 +0100

    light change

--- process/process_csv.py
+++ process/process_csv.py
@@ -13,7 +13,7 @@ def get_plot(movie, year="1995"):
     if page_py.exists():
         try:
             return str(
-                page_py.section_by_title("Plot").text
+                page_py.section_by_title("Plot").text[:500]
             )  # assure the output is a string
         except:
             return pd.NaT
@@ -39,11 +39,12 @@ def process_data(file):
     data["year"] = data["prompt"].str.extract(
         "(\d{4})", expand=True
     )  # extract the year
-    data["title"] = data["prompt"].map(
-        lambda x: x.split("(")[0]
-    )  # delete the name
+    data["title"] = data["prompt"].map(lambda x: x.split("(")[0])  # delete the name
     data["description"] = data["title"].map(lambda x: get_plot(x))  # get the plot
     data.drop("prompt", axis=1, inplace=True)
+    data["text"] = data["title"] + " : " + data["description"]
+    data.drop("title", axis=1, inplace=True)
+    data.drop("description", axis=1, inplace=True)
     data.dropna(inplace=True)  # drop the rows with no plot
     data.to_csv("../data/processed.csv", sep=",", index=False)
 
