827695f96db27864431c70e01188e4e421c6ff29
Yuuuugo <hugogabrielidis@gmail.com>
2023-03-05 16:32:17 +0100

    deleting bs in json

--- train.py
+++ train.py
@@ -24,11 +24,13 @@ if __name__ == "__main__":
         num_train_epochs=4.3,
         logging_steps=50,
         save_strategy=IntervalStrategy.NO,
+        per_device_train_batch_size=15,
+        per_device_eval_batch_size=15,
         warmup_steps=50,
         weight_decay=0.01,
         logging_dir="./logs",
         fp16=True,
-        deepspeed="./gpt_neo_ds_config.json",
+        deepspeed="./ds_config_gpt_neo_27.json",
         report_to="none",
     )
 
