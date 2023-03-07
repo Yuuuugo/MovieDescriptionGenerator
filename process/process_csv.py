76cfa61e00a9ac8552d206786cc62611388304a6
Yuuuugo <hugogabrielidis@gmail.com>
2023-03-07 16:42:04 +0100

    debug with axis = 1

--- process/process_csv.py
+++ process/process_csv.py
@@ -10,7 +10,8 @@ From the movies
 
 
 def summarize(text):
-    model = "philschmid/bart-large-cnn-samsum"
+    model = "t5-small"
+    # model = "philschmid/bart-large-cnn-samsum"
     summarizer = pipeline("summarization", model=model)
     summary_text = summarizer(text, max_length=300, min_length=250, do_sample=False)[0][
         "summary_text"
@@ -37,7 +38,7 @@ def get_plot(movie, year="1995"):
 
 def process_data(file):
     data = pd.read_csv(file, sep=",", names=["index", "prompt"])
-    data = data[:500]
+    data = data[:5]
     data.drop("index", axis=1, inplace=True)  # drop the index column
     data["year"] = data["prompt"].str.extract(
         "(\d{4})", expand=True
@@ -45,8 +46,9 @@ def process_data(file):
     data["title"] = data["prompt"].progress_map(
         lambda x: x.split("(")[0]
     )  # delete the name
-    data["description"] = data["title"].progress_map(
-        lambda x: get_plot(x)
+    # get the plot
+    data["description"] = data.progress_apply(
+        lambda x: get_plot(movie=x["title"], year=x["year"]), axis=1
     )  # get the plot
     data.drop("prompt", axis=1, inplace=True)
     data["text"] = data["title"] + " : " + data["description"]
