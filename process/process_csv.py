63a996d18862d612e6de136e2a6126f0cd0833d2
Yuuuugo <hugogabrielidis@gmail.com>
2023-03-07 16:14:14 +0100

    change to add summarize of entire plot & tqdm for pandas mapping

--- process/process_csv.py
+++ process/process_csv.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import wikipediaapi
+from transformers import pipeline
+from tqdm import tqdm
 
 
 """
@@ -7,29 +9,32 @@ From the movies.csv create a processed.csv file with the following columns: year
 """
 
 
+def summarize(text):
+    model = "philschmid/bart-large-cnn-samsum"
+    summarizer = pipeline("summarization", model=model)
+    summary_text = summarizer(text, max_length=300, min_length=250, do_sample=False)[0][
+        "summary_text"
+    ]
+    return summary_text
+
+
 def get_plot(movie, year="1995"):
     wiki_wiki = wikipediaapi.Wikipedia("en")
     page_py = wiki_wiki.page(movie)
     if page_py.exists():
         try:
-            return str(
-                page_py.section_by_title("Plot").text[:500]
-            )  # assure the output is a string
+            return summarize(str(page_py.section_by_title("Plot")))
         except:
             return pd.NaT
     else:
-        return pd.NaT
-
-
-"""     else:
-        page_py = wiki_wiki.page(movie + ' (' + year + ' film)')
+        page_py = wiki_wiki.page(movie + "(" + year + " film)")
         if page_py.exists():
             try:
-                print(movie)
-                return page_py.section_by_title('Plot').text[:500]
+                return summarize(str(page_py.section_by_title("Plot")))
             except:
-                print(movie)
-                return pd.NaT """
+                return pd.NaT
+        else:
+            return pd.NaT
 
 
 def process_data(file):
@@ -39,15 +44,20 @@ def process_data(file):
     data["year"] = data["prompt"].str.extract(
         "(\d{4})", expand=True
     )  # extract the year
-    data["title"] = data["prompt"].map(lambda x: x.split("(")[0])  # delete the name
-    data["description"] = data["title"].map(lambda x: get_plot(x))  # get the plot
+    data["title"] = data["prompt"].progress_map(
+        lambda x: x.split("(")[0]
+    )  # delete the name
+    data["description"] = data["title"].progress_map(
+        lambda x: get_plot(x)
+    )  # get the plot
     data.drop("prompt", axis=1, inplace=True)
     data["text"] = data["title"] + " : " + data["description"]
     data.drop("title", axis=1, inplace=True)
     data.drop("description", axis=1, inplace=True)
     data.dropna(inplace=True)  # drop the rows with no plot
-    data.to_csv("../data/processed.csv", sep=",", index=False)
+    data.to_csv("../data/summarized_movies.csv", sep=",", index=False)
 
 
 if __name__ == "__main__":
+    tqdm.pandas()
     process_data("../data/movies.csv")
